5238c565d1213fd850015e99e9a4d377e545bb67
Norberto Taveras <nm.taveras@gmail.com>
2019-12-19 20:09:18 -0500

    Movies & Shows View Controllers
    ADD - movies view controller, which will display all possible movies from the api.
    ADD - shows view controller, which will display all possible shows from the api.
    ADD - implementation of requesting and displaying more movies or shows from the api when scrolling down the table view.
    ADD - implementation of requesting and searching for specific movies and shows.
    ADD - implementation sorting movies and shows based on sort selection.
    ADD - implementation of title changed based on sort selection.

---
+++ Flixiago/Flixiago/Controllers/MoviesViewController.swift
@@ -0,0 +1,110 @@
+//
+//  MoviesViewController.swift
+//  Flixiago
+//
+//  Created by Norberto Taveras on 12/16/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: MediaViewBaseController {
+    
+    @IBOutlet weak var moviesTable: UITableView!
+    @IBOutlet weak var sortByView: UILabel!
+    @IBOutlet weak var sortBySegment: UISegmentedControl!
+    @IBOutlet weak var searchView: UISearchBar!
+    @IBOutlet weak var genreMenuView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        initSortBySegment(
+            sortBySegment: sortBySegment,
+            fromSortBys: TMDBService.movieSortBys,
+            titleView: sortByView)
+        
+        initTable(table: moviesTable)
+        initSearch(searchBar: searchView)
+        initGenreFilter(genreFilterButton: genreMenuView)
+        
+        changeSortBy(to: 0)
+        
+    }
+    
+    override func loadMore(page: Int, query: String?, genreId: Int64?) {
+        if let genreId = genreId {
+            TMDBService.searchMovies(
+                genreId: genreId,
+                page: page) { (movies, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let movies = movies {
+                        print("Response \(movies)")
+                        self.append(media: movies.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        } else if let query = query {
+            TMDBService.searchMovies(
+                query: query,
+                page: page) { (movies, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let movies = movies {
+                        print("Response \(movies)")
+                        self.append(media: movies.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        } else {
+            TMDBService.getMovies(
+                sortBy: TMDBService.movieSortBys[currentSortBy].sortBy,
+                page: page,
+                language: TMDBService.LANGUAGE) { (movies, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let movies = movies {
+                        print("Response \(movies)")
+                        self.append(media: movies.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        }
+    }
+    
+    @IBAction func sortByViewChanged(_ sender: UISegmentedControl) {
+        changeSortBy(to: sender.selectedSegmentIndex)
+    }
+    
+    override func titleFromSortBy(
+        pair: TMDBService.SortByPair) -> String {
+        
+        switch pair.sortBy {
+        case TMDBService.POPULAR:
+            return "Popular Movies"
+            
+        case TMDBService.TOP_RATED:
+            return "Top Rated Movies"
+            
+        case TMDBService.UPCOMING:
+            return "Upcoming Movies"
+            
+        default:
+            return super.titleFromSortBy(pair: pair)
+            
+        }
+    }
+    
+    override func getGenres(
+        callback: @escaping (TMDBService.GenreLookup?, Error?) -> Void) {
+        
+        TMDBService.getMovieGenres(callback: callback)
+    }
+
+}
---
+++ Flixiago/Flixiago/Controllers/ShowsViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ShowsViewController.swift
+//  Flixiago
+//
+//  Created by Norberto Taveras on 12/16/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+
+class ShowsViewController: MediaViewBaseController {
+
+    @IBOutlet weak var genresMenuView: UIImageView!
+    @IBOutlet weak var moviesTable: UITableView!
+    @IBOutlet weak var sortBySegment: UISegmentedControl!
+    @IBOutlet weak var searchBarView: UISearchBar!
+    @IBOutlet weak var sortByView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        initSortBySegment(sortBySegment: sortBySegment,
+                          fromSortBys: TMDBService.showSortBys,
+                          titleView: sortByView)
+        
+        initTable(table: moviesTable)
+        initSearch(searchBar: searchBarView)
+        initGenreFilter(genreFilterButton: genresMenuView)
+        
+        changeSortBy(to: 0)
+    }
+    
+    override func loadMore(page: Int, query: String?, genreId: Int64?) {
+        sortBySegment.isEnabled = (query == nil)
+        
+        if let genreId = genreId {
+            TMDBService.searchShows(
+                genreId: genreId,
+                page: page) { (shows, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let shows = shows {
+                        print("Response \(shows)")
+                        self.append(media: shows.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        } else if let query = query {
+            TMDBService.searchShows(
+                query: query,
+                page: page) { (shows, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let shows = shows {
+                        print("Response \(shows)")
+                        self.append(media: shows.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        } else {
+            TMDBService.getShows(
+                sortBy: TMDBService.showSortBys[currentSortBy].sortBy,
+                page: page,
+                language: TMDBService.LANGUAGE) { (shows, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                    } else if let shows = shows {
+                        print("Response \(shows)")
+                        self.append(media: shows.results)
+                    } else {
+                        print("nil response and no error!")
+                    }
+            }
+        }
+    }
+
+    @IBAction func sortByViewChanged(_ sender: UISegmentedControl) {
+        changeSortBy(to: sender.selectedSegmentIndex)
+    }
+    
+    override func titleFromSortBy(pair: TMDBService.SortByPair) -> String {
+        switch pair.sortBy {
+        case TMDBService.POPULAR:
+            return "Popular Shows"
+        case TMDBService.TOP_RATED:
+            return "Top Rated Shows"
+        default:
+            return super.titleFromSortBy(pair: pair)
+        }
+    }
+    
+    override func getGenres(
+        callback: @escaping (TMDBService.GenreLookup?, Error?) -> Void) {
+        
+        TMDBService.getShowGenres(callback: callback)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
