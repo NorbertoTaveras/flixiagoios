b839bcd09279f49f301b99bbbf69b5eef18844e8
Norberto Taveras <nm.taveras@gmail.com>
2020-01-12 16:43:31 -0500

    Favorite Record
    UPDATE - the favorite record database object model to support timestamp, media kind and favorite toggle.

--- Flixiago/Flixiago/Models/FavoriteRecord.swift
+++ Flixiago/Flixiago/Models/FavoriteRecord.swift
@@ -9,40 +9,58 @@
 import Foundation
 import RealmSwift
 
-class FavoriteRecord: Object {
-    @objc dynamic var type_id: String = ""
+public class FavoriteRecord: Object {
+    @objc dynamic var kind_type_id: String = ""
+    @objc dynamic var favorite: Bool = true
+    @objc dynamic var timestamp: Int64 = 0
     
     private static var changeListeners: [Int: () -> Void] = [:]
     private static var nextChangeListenerId: Int = 0
     
-    public typealias TypeIdPair = (type: String, id: Int64)
+    public typealias KindTypeId = (
+        kind: String,
+        type: String,
+        id: Int64
+    )
     
-    override class func primaryKey() -> String? {
-        return "type_id"
+    override public class func primaryKey() -> String? {
+        return "kind_type_id"
     }
     
-    func parseKey() -> TypeIdPair? {
-        guard let sepOfs = type_id.firstIndex(of: "_")
+    func parseKey() -> KindTypeId? {
+        guard let kindSepOfs = kind_type_id.firstIndex(of: "_")
             else { return nil }
         
-        let type = type_id[type_id.startIndex ..< sepOfs]
-        let idText = type_id[type_id.index(sepOfs, offsetBy: 1)...]
+        let kind = kind_type_id[kind_type_id.startIndex ..< kindSepOfs]
+        
+        let type_id = kind_type_id[kind_type_id.index(kindSepOfs, offsetBy: 1)...]
+        
+        guard let idSepOfs = type_id.firstIndex(of: "_")
+            else { return nil }
+        
+        let type = type_id[type_id.startIndex ..< idSepOfs]
+        
+        let idText = type_id[type_id.index(idSepOfs, offsetBy: 1)...]
         
         let result = (
+            kind: String(kind),
             type: String(type),
             id: Int64(String(idText))
-        ) as? FavoriteRecord.TypeIdPair
+        ) as? FavoriteRecord.KindTypeId
         
         return result
     }
     
     required init() {
-    }
-
-    init(type: String, id: Int64) {
-        self.type_id = "\(type)_\(id)"
+        super.init()
     }
     
+    init(kind: String, type: String, id: Int64, isFavorite: Bool) {
+        self.kind_type_id = "\(kind)_\(type)_\(id)"
+        self.favorite = isFavorite
+        self.timestamp = Int64(Date().timeIntervalSince1970 * 1000)
+    }
+
     public static func startListeningForChanges(
         callback: @escaping () -> Void) -> Int {
         
@@ -64,47 +82,119 @@ class FavoriteRecord: Object {
         }
     }
     
-    public static func getFavorite(type: String, id: Int64) -> FavoriteRecord? {
-        let realm = try! Realm()
+    public static func getFavorite(
+        type: String,
+        id: Int64) -> FavoriteRecord? {
         
-        let records = realm
-            .objects(FavoriteRecord.self)
-            .filter("type_id == \"\(type)_\(id)\"")
+        return get(
+            kind: "f",
+            type: type,
+            id: id)
+    }
+    
+    public static func getWatch(
+        type: String,
+        id: Int64) -> FavoriteRecord? {
         
-        return records.count > 0 ? records.first : nil
+        return get(
+            kind: "w",
+            type: type,
+            id: id)
     }
     
     public static func setFavorite(
         type: String,
         id: Int64,
-        isFavorite: Bool) {
+        isFavorite: Bool) -> FavoriteRecord? {
+        
+        return set(
+            kind: "f",
+            type: type,
+            id: id,
+            isFavorite: isFavorite)
+    }
+    
+    public static func setWatch(
+        type: String,
+        id: Int64,
+        isFavorite: Bool) -> FavoriteRecord? {
+        
+        return set(
+            kind: "w",
+            type: type,
+            id: id,
+            isFavorite: isFavorite)
+    }
+
+    public static func set(
+        kind: String,
+        type: String,
+        id: Int64,
+        isFavorite: Bool) -> FavoriteRecord? {
+        
+        return set(kind: kind,
+            type: type,
+            id: id,
+            isFavorite: isFavorite,
+            timestamp: nil)
+    }
+    
+    public static func set(
+        kind: String,
+        type: String,
+        id: Int64,
+        isFavorite: Bool,
+        timestamp: Int64?) -> FavoriteRecord? {
         
         let realm = try! Realm()
         
         var record: FavoriteRecord?
             
-        record = FavoriteRecord.getFavorite(type: type, id: id)
+        record = FavoriteRecord.get(
+            kind: kind,
+            type: type,
+            id: id)
         
         do {
             try realm.write {
                 if let existingRecord = record {
                     realm.delete(existingRecord)
-                } else {
-                    let record = FavoriteRecord(type: type, id: id)
-                    realm.add(record)
                 }
+                
+                let record = FavoriteRecord(
+                    kind: kind,
+                    type: type,
+                    id: id,
+                    isFavorite: isFavorite)
+                
+                if timestamp != nil {
+                    record.timestamp =
+                        Int64(Date().timeIntervalSince1970 * 1000)
+                }
+                
+                realm.add(record)
             }
         } catch {
             print(error.localizedDescription)
         }
         
         notifyChange()
+        
+        return record
     }
     
-    public static func getFavorites() -> [FavoriteRecord] {
+    public static func get(
+        kind: String,
+        type: String,
+        since: Int64) -> [FavoriteRecord] {
+        
         let realm = try! Realm()
         
         let records = realm.objects(FavoriteRecord.self)
+            .filter("kind_type_id BEGINSWITH \"\(kind)_\(type)\"" +
+                " && timestamp > \(since)")
+            .sorted(byKeyPath: "timestamp",
+                    ascending: false)
         
         var results: [FavoriteRecord] = []
         results.append(contentsOf: records)
@@ -112,11 +202,92 @@ class FavoriteRecord: Object {
         return results
     }
     
-    public static func setupButton(
+    public static func getFavorites(onlyFavorite: Bool) -> [FavoriteRecord] {
+        return get(kind: "f",
+                   onlyFavorite: onlyFavorite)
+    }
+    
+    public static func getWatches(onlyFavorite: Bool) -> [FavoriteRecord] {
+        return get(kind: "w",
+                   onlyFavorite: onlyFavorite)
+    }
+
+    public static func get(kind: String, onlyFavorite: Bool) -> [FavoriteRecord] {
+        let realm = try! Realm()
+        
+        var records = realm.objects(FavoriteRecord.self)
+            .filter("kind_type_id BEGINSWITH \"\(kind)_\"")
+        
+        if onlyFavorite {
+            records = records.filter("favorite != false")
+        }
+        
+        var results: [FavoriteRecord] = []
+        results.append(contentsOf: records)
+        
+        return results
+    }
+    
+    public static func get(
+        kind: String,
+        type: String,
+        id: Int64) -> FavoriteRecord? {
+        let realm = try! Realm()
+        
+        let records = realm.objects(FavoriteRecord.self)
+            .filter("kind_type_id == \"\(kind)_\(type)_\(id)\"")
+        
+        return records.first
+    }
+    
+    public static func setupFavoriteButton(
         type: String,
         id: Int64,
-        into view: UIButton) {
+        into view: UIButton) -> FavoriteRecord? {
         
+        return setupButton(
+            kind: "f",
+            type: type,
+            id: id,
+            into: view)
+    }
+    
+    public static func setupWatchButton(
+        type: String,
+        id: Int64,
+        into view: UIButton) -> FavoriteRecord? {
+        
+        return setupButton(
+            kind: "w",
+            type: type,
+            id: id,
+            into: view)
+    }
+    
+    public static func setupButton(
+        kind: String,
+        type: String,
+        id: Int64,
+        into view: UIButton) -> FavoriteRecord? {
+        let record = FavoriteRecord.get(
+            kind: kind,
+            type: type,
+            id: id)
+        
+        setupButton(
+            kind: kind,
+            type: type,
+            record: record,
+            into: view)
+        
+        return record
+    }
+    
+    public static func setupButton(
+        kind: String,
+        type: String,
+        record: FavoriteRecord?,
+        into view: UIButton) {
         let purple = UIColor(
             red: CGFloat(0x4e) / 255.0,
             green: CGFloat(0x32) / 255.0,
@@ -125,7 +296,7 @@ class FavoriteRecord: Object {
         
         view.layer.cornerRadius = 8.0;
         
-        if let _ = FavoriteRecord.getFavorite(type: type, id: id) {
+        if record?.favorite ?? false {
             view.backgroundColor = purple
             view.setTitleColor(UIColor.white, for: .normal)
             view.tintColor = UIColor.white
@@ -138,18 +309,71 @@ class FavoriteRecord: Object {
         }
     }
     
-    public static func setupButton(
+    private static func iconsFrom(kind: String) -> [String] {
+        switch kind {
+        case "f":
+            return [
+                "heart",
+                "heart.fill"
+            ]
+            
+        case "w":
+            return [
+                "eye",
+                "eye.fill"
+            ]
+            
+        default:
+            return []
+        }
+    }
+    
+    public static func setupFavoriteButton(
         type: String,
         id: Int64,
-        into view: UIImageView) {
+        into view: UIImageView) -> FavoriteRecord? {
+        
+        return setupButton(
+            kind: "f",
+            type: type,
+            id: id,
+            into: view)
+    }
+    
+    public static func setupWatchButton(
+        type: String,
+        id: Int64,
+        into view: UIImageView) -> FavoriteRecord? {
+        
+        return setupButton(
+            kind: "w",
+            type: type,
+            id: id,
+            into: view)
+    }
+
+    public static func setupButton(
+        kind: String,
+        type: String,
+        id: Int64,
+        into view: UIImageView) -> FavoriteRecord? {
         
         view.layer.cornerRadius = 8;
         
-        if let _ = FavoriteRecord.getFavorite(type: type, id: id) {
-            view.image = UIImage(systemName: "heart.fill")
+        let icons = iconsFrom(kind: kind)
+        
+        let record = FavoriteRecord.get(
+            kind: kind,
+            type: type,
+            id: id)
+        
+        if record?.favorite ?? false {
+            view.image = UIImage(systemName: icons[1])
         } else {
-            view.image = UIImage(systemName: "heart")
+            view.image = UIImage(systemName: icons[0])
         }
+        
+        return record
     }
 }
 
